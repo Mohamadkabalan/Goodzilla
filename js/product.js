07cb32497a968d5d014f86fb0317f0e537d09eec
anthony tahtouh <anthony1.tahtouh@gmail.com>
2019-10-09 02:04:43 +0300

    product page done with the tabs section and two tab pages insidefirst and third

--- js/product.js
+++ js/product.js
@@ -1,3 +1,31 @@
+$(document).ready(function () {
+    $(".product_tab li a").click(function (event) {
+        event.preventDefault();
+        $(".product_tab li a").removeClass('active');
+        $(this).addClass('active');
+        var $id = $(this).attr('id');
+        var $sub = $('#sub_' + $id);
+        $('.product_contenttabs').hide();
+        $sub.show();
+    });
+    $(document).on('click', '.close_add_but_first_ad', function () {
+        $(".first_add_container").hide();
+    });
+    $(document).on('click', '.close_add_but_second_ad', function () {
+        $(".second_add_container").hide();
+    });
+
+    $('#stars').on('starrr:change', function (e, value) {
+        $('#count').html(value);
+    });
+
+    $('#stars-existing').on('starrr:change', function (e, value) {
+        $('#count-existing').html(value);
+    });
+
+});
+
+
 $('.btn-number').click(function (e) {
     e.preventDefault();
 
@@ -69,3 +97,110 @@ $(".input-number").keydown(function (e) {
                 e.preventDefault();
             }
         });
+
+
+var __slice = [].slice;
+
+(function ($, window) {
+    var Starrr;
+
+    Starrr = (function () {
+        Starrr.prototype.defaults = {
+            rating: void 0,
+            numStars: 5,
+            change: function (e, value) {}
+        };
+
+        function Starrr($el, options) {
+            var i, _, _ref,
+                    _this = this;
+
+            this.options = $.extend({}, this.defaults, options);
+            this.$el = $el;
+            _ref = this.defaults;
+            for (i in _ref) {
+                _ = _ref[i];
+                if (this.$el.data(i) != null) {
+                    this.options[i] = this.$el.data(i);
+                }
+            }
+            this.createStars();
+            this.syncRating();
+            this.$el.on('mouseover.starrr', 'span', function (e) {
+                return _this.syncRating(_this.$el.find('span').index(e.currentTarget) + 1);
+            });
+            this.$el.on('mouseout.starrr', function () {
+                return _this.syncRating();
+            });
+            this.$el.on('click.starrr', 'span', function (e) {
+                return _this.setRating(_this.$el.find('span').index(e.currentTarget) + 1);
+            });
+            this.$el.on('starrr:change', this.options.change);
+        }
+
+        Starrr.prototype.createStars = function () {
+            var _i, _ref, _results;
+
+            _results = [];
+            for (_i = 1, _ref = this.options.numStars; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
+                _results.push(this.$el.append("<span class='eval_options_glob empty_stars'></span>"));
+            }
+            return _results;
+        };
+
+        Starrr.prototype.setRating = function (rating) {
+            if (this.options.rating === rating) {
+                rating = void 0;
+            }
+            this.options.rating = rating;
+            this.syncRating();
+            return this.$el.trigger('starrr:change', rating);
+        };
+
+        Starrr.prototype.syncRating = function (rating) {
+            var i, _i, _j, _ref;
+
+            rating || (rating = this.options.rating);
+            if (rating) {
+                for (i = _i = 0, _ref = rating - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+                    this.$el.find('span').eq(i).removeClass('empty_stars').addClass('red-star');
+                }
+            }
+            if (rating && rating < 5) {
+                for (i = _j = rating; rating <= 4 ? _j <= 4 : _j >= 4; i = rating <= 4 ? ++_j : --_j) {
+                    this.$el.find('span').eq(i).removeClass('red-star').addClass('empty_stars');
+                }
+            }
+            if (!rating) {
+                return this.$el.find('span').removeClass('red-star').addClass('empty_stars');
+            }
+        };
+
+        return Starrr;
+
+    })();
+    return $.fn.extend({
+        starrr: function () {
+            var args, option;
+
+            option = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+            return this.each(function () {
+                var data;
+
+                data = $(this).data('star-rating');
+                if (!data) {
+                    $(this).data('star-rating', (data = new Starrr($(this), option)));
+                }
+                if (typeof option === 'string') {
+                    return data[option].apply(data, args);
+                }
+            });
+        }
+    });
+})(window.jQuery, window);
+
+$(function () {
+    return $(".starrr").starrr();
+});
+
+		
