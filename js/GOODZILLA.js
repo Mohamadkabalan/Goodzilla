9ad73f6e226a068d1f783b2f4cd3c8f608bd72b7
anthony tahtouh <anthony1.tahtouh@gmail.com>
2019-10-11 02:52:41 +0300

    home page cart drop down add responsive

--- js/GOODZILLA.js
+++ js/GOODZILLA.js
@@ -10,8 +10,20 @@ $(document).ready(function () {
         $('#replacable_text').val(variableProduct);
         $(".list_of_product_hidden_container").hide();
     });
-
-      var swiper = new Swiper('.swiper-container', {
+    $(document).on('click', '.cart_close_but', function () {
+        $("#cart_absolute_container").hide();
+    });
+    $(document).on('mouseover', '.carts_list_element', function () {
+        $("#cart_absolute_container").show();
+    });
+    $(document).on('mouseout', '.carts_list_element', function () {
+        $("#cart_absolute_container").hide();
+    });
+    $('p').filter(function () {
+        return $.trim(this.innerHTML) == ""
+    }).remove();
+    $("div").find(".item_in_cart_container:last").css("border-top", "none");
+    var swiper = new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 0,
         slidesPerGroup: 1,
@@ -21,23 +33,23 @@ $(document).ready(function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-		breakpoints: {
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-        },
-		767: {
-          slidesPerView: 2,
-          spaceBetween: 0,
-        },
-        991: {
-          slidesPerView: 3,
-          spaceBetween: 0,
-        },
-        9999: {
-          slidesPerView: 4,
-          spaceBetween: 0,
-        },
-      }
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 0,
+            },
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 0,
+            },
+            9999: {
+                slidesPerView: 4,
+                spaceBetween: 0,
+            },
+        }
     });
 });
